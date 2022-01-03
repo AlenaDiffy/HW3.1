1fb0c96aaac358b397a5ad32d9fc3d08ca6d878c
AlenaDiffy <alenadernova@gmail.com>
2022-01-04 01:26:18 +0300

    FirstVersion

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate (int Price) {
+        int price = 1_000;
+        int milesReward = 20;
+        int Miles = price / milesReward;
+         return Miles;
+    }
+
+}
