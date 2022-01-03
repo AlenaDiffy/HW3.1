d25a17c8788d8fdfcab5c20784a5a973c395cb61
AlenaDiffy <alenadernova@gmail.com>
2022-01-04 01:42:59 +0300

    ReformCode

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,9 @@
 public class BonusMilesService {
-    public int calculate (int Price) {
+    public int calculate(int Price) {
         int price = 1_000;
         int milesReward = 20;
         int Miles = price / milesReward;
-         return Miles;
+        return Miles;
     }
 
 }
